523f8debca48862b3da808a1a69464567c7b8e9d
kibunjaa <kibunjafranziska@gmail.com>
2023-10-05 22:06:38 +0300

    Creating my maps using python

---
+++ Limuru.py
@@ -0,0 +1,4 @@
+import folium
+
+nakuru = folium.Map(location=[-1.1000, 36.6500], zoom_start=20)
+nakuru.save("Limuru.html")
---
+++ Nakuru.py
@@ -0,0 +1,4 @@
+import folium
+
+nakuru = folium.Map(location=[-0.303099, 36.080025], zoom_start=15)
+nakuru.save("Nakuru.html")
